29a3d56dd67e0f7902ec61b4a1c09b2d8f640190
HERZY <HERZY@Herzy-PC>
2015-12-15 20:34:09 +0100

    multicast msg

--- src/messanger/Messanger.java
+++ src/messanger/Messanger.java
@@ -6,7 +6,11 @@
 package messanger;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  *
@@ -26,30 +30,51 @@ public class Messanger {
      */
     public static void main(String[] args) {
         
-        try {
-                EchoWorker worker = new EchoWorker();
-                new Thread(worker).start();
-                new Thread(new NioServer(null, 9090, worker)).start();
+        startReceiver();
+     
+        try 
+        {
+            InetAddress group = InetAddress.getByName("224.0.0.251");
+            MulticastSocket s = new MulticastSocket(5353);
+            s.joinGroup(group);
+
+            Scanner reader = new Scanner(System.in);
+            while(true){
+                System.out.println("Write message: ");
+                String msg = reader.nextLine();
+                DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(),group, 5353);
+                s.send(packet);
+
+            }
         } catch (IOException e) {
-                e.printStackTrace();
-        }
-
-
-
-        try {
-                NioClient client = new NioClient(InetAddress.getByName("www.google.com"), 80);
-                Thread t = new Thread(client);
-                t.setDaemon(true);
-                t.start();
-                RspHandler handler = new RspHandler();
-                client.send("GET / HTTP/1.0\r\n\r\n".getBytes(), handler);
-                handler.waitForResponse();
-        } catch (Exception e) {
-                e.printStackTrace();
+            e.printStackTrace();
         }
     }
     
     
-    
-    
+     public static void startReceiver() {
+        (new Thread() {
+            @Override
+            public void run() {
+                try{
+                    InetAddress group = InetAddress.getByName("224.0.0.251");
+                    MulticastSocket s = new MulticastSocket(5353);
+                    s.joinGroup(group);
+  
+                    while (true) {
+                        byte[] buf = new byte[1000];
+                        DatagramPacket recv = new DatagramPacket(buf, buf.length);
+                        s.receive(recv);
+                        String msg = new String(buf);
+                        System.err.println("Received " + recv.getLength() +" bytes from " + recv.getAddress() + " " + msg);
+                    }
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } 
+            }
+        }).start();
+    }
 }
